38d966e936ad33c0bb1a090af75094594545535a
NAVERON <2046783134@qq.com>
2018-09-28 10:03:04 +0800

    ......

--- MachineLearning/TensorflowLearn/MoFan/Demo/Robot_arm/A3C.py
+++ MachineLearning/TensorflowLearn/MoFan/Demo/Robot_arm/A3C.py
@@ -1,73 +1,86 @@
-
-
-
 """
-Asynchronous Advantage Actor Critic (A3C) with continuous action space, Reinforcement Learning.
-The Pendulum example. Convergence promised, but difficult environment, this code hardly converge.
+Environment is a Robot Arm. The arm tries to get to the blue point.
+The environment will return a geographic (distance) information for the arm to learn.
+The far away from blue point the less reward; touch blue r+=1; stop at blue for a while then get r=+10.
+ 
+You can train this RL by using LOAD = False, after training, this model will be store in the a local folder.
+Using LOAD = True to reload the trained model for playing.
+You can customize this script in a way you want.
 View more on [莫烦Python] : https://morvanzhou.github.io/tutorials/
-Using:
-tensorflow 1.0
-gym 0.8.0
+Requirement:
+pyglet >= 1.2.4
+numpy >= 1.12.1
+tensorflow >= 1.0.1
 """
 
 import multiprocessing
 import threading
 import tensorflow as tf
 import numpy as np
-import gym
-import os
-import shutil
-import matplotlib.pyplot as plt
+from MoFan.Demo.Robot_arm.arm_env import ArmEnv
 
 
-GAME = 'LunarLander-v2'
-OUTPUT_GRAPH = False
-LOG_DIR = './log'
-N_WORKERS = multiprocessing.cpu_count()
-MAX_GLOBAL_EP = 5000
-GLOBAL_NET_SCOPE = 'Global_Net'
+# np.random.seed(1)
+# tf.set_random_seed(1)
+
+MAX_GLOBAL_EP = 2000
+MAX_EP_STEP = 300
 UPDATE_GLOBAL_ITER = 5
-GAMMA = 0.99
-ENTROPY_BETA = 0.001   # not useful in this case
-LR_A = 0.0005    # learning rate for actor
-LR_C = 0.001    # learning rate for critic
+N_WORKERS = multiprocessing.cpu_count()
+LR_A = 1e-4  # learning rate for actor
+LR_C = 2e-4  # learning rate for critic
+GAMMA = 0.9  # reward discount
+MODE = ['easy', 'hard']
+n_model = 1
+GLOBAL_NET_SCOPE = 'Global_Net'
+ENTROPY_BETA = 0.01
 GLOBAL_RUNNING_R = []
 GLOBAL_EP = 0
 
-env = gym.make(GAME)
 
-N_S = env.observation_space.shape[0]
-N_A = env.action_space.n
+env = ArmEnv(mode=MODE[n_model])
+N_S = env.state_dim
+N_A = env.action_dim
+A_BOUND = env.action_bound
 del env
 
 
 class ACNet(object):
     def __init__(self, scope, globalAC=None):
+
         if scope == GLOBAL_NET_SCOPE:   # get global network
             with tf.variable_scope(scope):
                 self.s = tf.placeholder(tf.float32, [None, N_S], 'S')
-                self._build_net(N_A)
+                self._build_net()
                 self.a_params = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=scope + '/actor')
                 self.c_params = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=scope + '/critic')
         else:   # local net, calculate losses
             with tf.variable_scope(scope):
                 self.s = tf.placeholder(tf.float32, [None, N_S], 'S')
-                self.a_his = tf.placeholder(tf.int32, [None, ], 'A')
+                self.a_his = tf.placeholder(tf.float32, [None, N_A], 'A')
                 self.v_target = tf.placeholder(tf.float32, [None, 1], 'Vtarget')
 
-                self.a_prob, self.v = self._build_net(N_A)
+                mu, sigma, self.v = self._build_net()
 
                 td = tf.subtract(self.v_target, self.v, name='TD_error')
                 with tf.name_scope('c_loss'):
                     self.c_loss = tf.reduce_mean(tf.square(td))
 
+                with tf.name_scope('wrap_a_out'):
+                    self.test = sigma[0]
+                    mu, sigma = mu * A_BOUND[1], sigma + 1e-5
+
+                normal_dist = tf.contrib.distributions.Normal(mu, sigma)
+
                 with tf.name_scope('a_loss'):
-                    log_prob = tf.reduce_sum(tf.log(self.a_prob) * tf.one_hot(self.a_his, N_A, dtype=tf.float32), axis=1, keep_dims=True)
+                    log_prob = normal_dist.log_prob(self.a_his)
                     exp_v = log_prob * td
-                    entropy = -tf.reduce_sum(self.a_prob * tf.log(self.a_prob), axis=1, keep_dims=True)  # encourage exploration
+                    entropy = normal_dist.entropy()  # encourage exploration
                     self.exp_v = ENTROPY_BETA * entropy + exp_v
                     self.a_loss = tf.reduce_mean(-self.exp_v)
 
+                with tf.name_scope('choose_a'):  # use local params to choose action
+                    self.A = tf.clip_by_value(tf.squeeze(normal_dist.sample(1), axis=0), *A_BOUND)
                 with tf.name_scope('local_grad'):
                     self.a_params = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=scope + '/actor')
                     self.c_params = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=scope + '/critic')
@@ -82,109 +95,89 @@ class ACNet(object):
                     self.update_a_op = OPT_A.apply_gradients(zip(self.a_grads, globalAC.a_params))
                     self.update_c_op = OPT_C.apply_gradients(zip(self.c_grads, globalAC.c_params))
 
-    def _build_net(self, n_a):
-        w_init = tf.random_normal_initializer(0., .01)
-        with tf.variable_scope('critic'):
-            cell_size = 64
-            s = tf.expand_dims(self.s, axis=1,
-                               name='timely_input')  # [time_step, feature] => [time_step, batch, feature]
-            rnn_cell = tf.contrib.rnn.BasicRNNCell(cell_size)
-            self.init_state = rnn_cell.zero_state(batch_size=1, dtype=tf.float32)
-            outputs, self.final_state = tf.nn.dynamic_rnn(
-                cell=rnn_cell, inputs=s, initial_state=self.init_state, time_major=True)
-            cell_out = tf.reshape(outputs, [-1, cell_size], name='flatten_rnn_outputs')  # joined state representation
-            l_c = tf.layers.dense(cell_out, 200, tf.nn.relu6, kernel_initializer=w_init, name='lc')
-            v = tf.layers.dense(l_c, 1, kernel_initializer=w_init, name='v')  # state value
+    def _build_net(self):
+        w_init = tf.contrib.layers.xavier_initializer()
         with tf.variable_scope('actor'):
-            cell_out = tf.stop_gradient(cell_out, name='c_cell_out')
-            l_a = tf.layers.dense(cell_out, 300, tf.nn.relu6, kernel_initializer=w_init, name='la')
-            a_prob = tf.layers.dense(l_a, n_a, tf.nn.softmax, kernel_initializer=w_init, name='ap')
-
-        return a_prob, v
+            l_a = tf.layers.dense(self.s, 400, tf.nn.relu6, kernel_initializer=w_init, name='la')
+            l_a = tf.layers.dense(l_a, 300, tf.nn.relu6, kernel_initializer=w_init, name='la2')
+            mu = tf.layers.dense(l_a, N_A, tf.nn.tanh, kernel_initializer=w_init, name='mu')
+            sigma = tf.layers.dense(l_a, N_A, tf.nn.softplus, kernel_initializer=w_init, name='sigma')
+        with tf.variable_scope('critic'):
+            l_c = tf.layers.dense(self.s, 400, tf.nn.relu6, kernel_initializer=w_init, name='lc')
+            l_c = tf.layers.dense(l_c, 200, tf.nn.relu6, kernel_initializer=w_init, name='lc2')
+            v = tf.layers.dense(l_c, 1, kernel_initializer=w_init, name='v')  # state value
+        return mu, sigma, v
 
     def update_global(self, feed_dict):  # run by a local
-        SESS.run([self.update_a_op, self.update_c_op], feed_dict)  # local grads applies to global net
+        _, _, t = SESS.run([self.update_a_op, self.update_c_op, self.test], feed_dict)  # local grads applies to global net
+        return t
 
     def pull_global(self):  # run by a local
         SESS.run([self.pull_a_params_op, self.pull_c_params_op])
 
-    def choose_action(self, s, cell_state):  # run by a local
-        prob_weights, cell_state = SESS.run([self.a_prob, self.final_state], feed_dict={self.s: s[np.newaxis, :],
-                                                                            self.init_state: cell_state})
-        action = np.random.choice(range(prob_weights.shape[1]),
-                                  p=prob_weights.ravel())  # select action w.r.t the actions prob
-        return action, cell_state
+    def choose_action(self, s):  # run by a local
+        s = s[np.newaxis, :]
+        return SESS.run(self.A, {self.s: s})[0]
 
 
 class Worker(object):
     def __init__(self, name, globalAC):
-        self.env = gym.make(GAME)
+        self.env = ArmEnv(mode=MODE[n_model])
         self.name = name
         self.AC = ACNet(name, globalAC)
 
     def work(self):
         global GLOBAL_RUNNING_R, GLOBAL_EP
         total_step = 1
-        r_scale = 100
         buffer_s, buffer_a, buffer_r = [], [], []
         while not COORD.should_stop() and GLOBAL_EP < MAX_GLOBAL_EP:
             s = self.env.reset()
             ep_r = 0
-            ep_t = 0
-            rnn_state = SESS.run(self.AC.init_state)  # zero rnn state at beginning
-            keep_state = rnn_state.copy()  # keep rnn state for updating global net
-            while True:
-                # if self.name == 'W_0' and total_step % 10 == 0:
-                #     self.env.render()
-                a, rnn_state_ = self.AC.choose_action(s, rnn_state)  # get the action and next rnn state
-                s_, r, done, info = self.env.step(a)
-                if r == -100: r = -10
+            for ep_t in range(MAX_EP_STEP):
+                if self.name == 'W_0':
+                    self.env.render()
+                a = self.AC.choose_action(s)
+                s_, r, done = self.env.step(a)
+                if ep_t == MAX_EP_STEP - 1: done = True
                 ep_r += r
                 buffer_s.append(s)
                 buffer_a.append(a)
-                buffer_r.append(r/r_scale)
+                buffer_r.append(r)
 
                 if total_step % UPDATE_GLOBAL_ITER == 0 or done:   # update global and assign to local net
                     if done:
                         v_s_ = 0   # terminal
                     else:
-                        v_s_ = SESS.run(self.AC.v, {self.AC.s: s_[np.newaxis, :], self.AC.init_state: rnn_state_})[0,0]
+                        v_s_ = SESS.run(self.AC.v, {self.AC.s: s_[np.newaxis, :]})[0, 0]
                     buffer_v_target = []
                     for r in buffer_r[::-1]:    # reverse buffer r
                         v_s_ = r + GAMMA * v_s_
                         buffer_v_target.append(v_s_)
                     buffer_v_target.reverse()
 
-                    buffer_s, buffer_a, buffer_v_target = np.vstack(buffer_s), np.array(buffer_a), np.vstack(buffer_v_target)
+                    buffer_s, buffer_a, buffer_v_target = np.vstack(buffer_s), np.vstack(buffer_a), np.vstack(buffer_v_target)
                     feed_dict = {
                         self.AC.s: buffer_s,
                         self.AC.a_his: buffer_a,
                         self.AC.v_target: buffer_v_target,
-                        self.AC.init_state: keep_state,
                     }
-
-                    self.AC.update_global(feed_dict)
-
+                    test = self.AC.update_global(feed_dict)
                     buffer_s, buffer_a, buffer_r = [], [], []
                     self.AC.pull_global()
-                    keep_state = rnn_state_.copy()  # replace the keep_state as the new initial rnn state_
 
                 s = s_
                 total_step += 1
-                rnn_state = rnn_state_  # renew rnn state
-                ep_t += 1
                 if done:
                     if len(GLOBAL_RUNNING_R) == 0:  # record running episode reward
                         GLOBAL_RUNNING_R.append(ep_r)
                     else:
-                        GLOBAL_RUNNING_R.append(0.99 * GLOBAL_RUNNING_R[-1] + 0.01 * ep_r)
-                    if not self.env.unwrapped.lander.awake: solve = '| Landed'
-                    else: solve = '| ------'
+                        GLOBAL_RUNNING_R.append(0.9 * GLOBAL_RUNNING_R[-1] + 0.1 * ep_r)
                     print(
                         self.name,
                         "Ep:", GLOBAL_EP,
-                        solve,
                         "| Ep_r: %i" % GLOBAL_RUNNING_R[-1],
+                        '| Var:', test,
+
                           )
                     GLOBAL_EP += 1
                     break
@@ -201,14 +194,9 @@ if __name__ == "__main__":
         for i in range(N_WORKERS):
             i_name = 'W_%i' % i   # worker name
             workers.append(Worker(i_name, GLOBAL_AC))
-
+            
     COORD = tf.train.Coordinator()
     SESS.run(tf.global_variables_initializer())
-
-    if OUTPUT_GRAPH:
-        if os.path.exists(LOG_DIR):
-            shutil.rmtree(LOG_DIR)
-        tf.summary.FileWriter(LOG_DIR, SESS.graph)
 
     worker_threads = []
     for worker in workers:
@@ -216,12 +204,12 @@ if __name__ == "__main__":
         t = threading.Thread(target=job)
         t.start()
         worker_threads.append(t)
+    
     COORD.join(worker_threads)
-
-    plt.plot(np.arange(len(GLOBAL_RUNNING_R)), GLOBAL_RUNNING_R)
-    plt.xlabel('step')
-    plt.ylabel('Total moving reward')
-    plt.show()
+    
+    
+    
+    
 
 
 
