aa9446ea8436373b204f6ed5e44ed209f2a7688a
NAVERON <2478327261@qq.com>
2018-10-07 22:04:48 +0800

    spride travel

--- Algorithms/travel_matrix.cpp
+++ Algorithms/travel_matrix.cpp
@@ -6,12 +6,13 @@
 #include <iostream>
 #include <stdlib.h>
 #include <numeric>
+#include <vector>
 
 class Solution          //解决螺旋遍历问题
 {
     public:
-    vector<int> findDiagonalOrder(vector<vector<int>>& matrix) {  //对角线遍历
-        vector<int> travel;
+    vector<int> findDiagonalOrder(vector<vector<int>>& matrix) {  //螺旋遍历算法实现
+        vector<int> travel;   //存放遍历结果
         if(matrix.empty()) return travel;
 
         int row = matrix.size();
@@ -19,12 +20,12 @@ class Solution          //解决螺旋遍历问题
 
         int row_begin = 0, row_end = row - 1;
         int col_begin = 0, col_end = cow - 1;
-        while(row_begin <= row_end && col_begin <= cow_end)
+        while(row_begin <= row_end && col_begin <= col_end)
         {
             //向右遍历
             for(int j = col_begin; j < col_end; j++)
             {
-                travel.push_back(matrix[row_begin][j]);
+                travel.push_back(matrix[row_begin][j]);  //把matrix值压入travel
             }
             row_begin--;
             //向下遍历
@@ -61,12 +62,54 @@ class Solution          //解决螺旋遍历问题
 }
 
 
-class Solution2
+class Solution2    //沿对角线来回遍历
 {
+    // 索引奇数偶数分类
+    // 为偶数。。。
+    // 为奇数。。。
     public:
-    vector<int> findTriOrdor(vector<vector<int>>& matrix)
+    vector<int> findTriOrdor(vector<vector<int>>& matrix)    //对角线遍历算法实现
     {
-        //
+        vector<int> travel;
+        if(matrix.empty()) return travel;
+
+        int row = matrix.size();
+        int col = matrix[0].size();
+
+        int row_index = 0, col_index = 0;
+        for(int i = 0; i < row*col; i++ )
+        {
+            travel.push_back(matrix[row_index][col_index]);
+            if((row_index + col_index)%2 == 0)
+            {
+                if(row_index == 0)
+                {
+                    col_index++;
+                }else if(col_index == col-1)
+                {
+                    row_index++;
+                }else
+                {
+                    row_index--;
+                    col_index++;
+                }
+            }
+            else
+            {
+                if(col_index == 0)
+                {
+                    row_index++;
+                }else if(row_index == row-1)
+                {
+                    col_index++;
+                }else
+                {
+                    row_index++;
+                    col_index--;
+                }
+            }
+        }
+        return travel;
     }
 }
 
